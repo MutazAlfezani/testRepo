1367e0437bd5fd1288a4af2030e7b5210b99fb17
MutazAlfezani <mutazalfezani@gmail.com>
2019-10-11 10:32:24 +0200

    added login activity

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,18 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-
-        }
+        setContentView(R.layout.activity_login)
     }
-
-
 }
-
