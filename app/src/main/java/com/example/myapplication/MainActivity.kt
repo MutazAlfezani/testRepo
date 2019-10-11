dc9685a47349f145af7f416dee84486d15954a48
MutazAlfezani <mutazalfezani@gmail.com>
2019-10-11 10:37:33 +0200

    change login layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,11 +9,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("hello")
         button.setOnClickListener {
 
         }
     }
 
+    override fun onRestart() {
+        super.onRestart()
+    }
 
 }
 
